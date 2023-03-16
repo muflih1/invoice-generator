0509b466f66427a8f601b9dc5a181ff00fe27b5c
Muflih <77203403+muflih1@users.noreply.github.com>
2023-03-16 15:28:14 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.hello,name='home'),
-    path('index/fhlshufe',views.index,name='index'),
+    path('index/',views.index,name='index'),
     path('buy/<int:pk>/',views.buy,name='buy'),
     path('atc/<int:pk>/',views.atc,name='atc'),
     path('pdf/',views.pdf,name='pdf'),
